6224a02a1b1a695dfcef29a1522b7fea17da547f
lvxin <lvxin0315@163.com>
2020-06-27 17:37:39 +0800

    处理答题内容

--- handlers/wechat.go
+++ handlers/wechat.go
@@ -1,9 +1,9 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/lvxin0315/gg/etc"
+	"github.com/lvxin0315/gg/services"
 	"github.com/silenceper/wechat/v2"
 	wxCache "github.com/silenceper/wechat/v2/cache"
 	"github.com/silenceper/wechat/v2/officialaccount"
@@ -14,6 +14,18 @@ import (
 )
 
 var ot *officialaccount.OfficialAccount
+
+const (
+	WxOptionA    = "A"
+	WxOptionB    = "B"
+	WxOptionC    = "C"
+	WxOptionD    = "D"
+	WxOptionE    = "E"
+	WxJudgeTrue  = "对"
+	WxJudgeFalse = "错"
+	WxBegin      = "wx_begin"    //开始答题
+	WxMyScore    = "wx_my_score" //我的战绩
+)
 
 //微信接口
 func WeChat(c *gin.Context) {
@@ -74,40 +86,77 @@ func wxMessageFunc(msg wxMessage.MixMessage) *wxMessage.Reply {
 func InitMenu() {
 	officialAccount := officialAccount()
 	m := officialAccount.GetMenu()
-
-	//测试点击
-	btn1 := new(menu.Button)
-	btn1.SetClickButton("测试1", "test1")
-	btn2 := new(menu.Button)
-	btn2.SetClickButton("测试2", "test2")
-	//测试二级菜单
-	btn3 := new(menu.Button)
-	btn31 := new(menu.Button)
-	btn31.SetClickButton("测试31", "test31")
-	btn32 := new(menu.Button)
-	btn32.SetClickButton("测试32", "test32")
-	btn33 := new(menu.Button)
-	btn33.SetClickButton("测试33", "test33")
-	btn3.SetSubButton("测试3", []*menu.Button{
-		btn31,
-		btn32,
-		btn33,
+	//选择题快捷键
+	choiceBtn := new(menu.Button)
+	choiceABtn := new(menu.Button)
+	choiceBBtn := new(menu.Button)
+	choiceCBtn := new(menu.Button)
+	choiceDBtn := new(menu.Button)
+	choiceEBtn := new(menu.Button)
+	choiceBtn.SetSubButton("选择键", []*menu.Button{
+		choiceABtn,
+		choiceBBtn,
+		choiceCBtn,
+		choiceDBtn,
+		choiceEBtn,
 	})
+	choiceABtn.SetClickButton("选项A", WxOptionA)
+	choiceBBtn.SetClickButton("选项B", WxOptionB)
+	choiceCBtn.SetClickButton("选项C", WxOptionC)
+	choiceDBtn.SetClickButton("选项D", WxOptionD)
+	choiceEBtn.SetClickButton("选项E", WxOptionE)
+	//判断题快捷键
+	judgeBtn := new(menu.Button)
+	judgeTrueBtn := new(menu.Button)
+	judgeFalseBtn := new(menu.Button)
+	judgeBtn.SetSubButton("判断键", []*menu.Button{
+		judgeTrueBtn,
+		judgeFalseBtn,
+	})
+	judgeTrueBtn.SetClickButton("对", WxJudgeTrue)
+	judgeFalseBtn.SetClickButton("错", WxJudgeFalse)
+	//其他
+	otherBtn := new(menu.Button)
+	//开始答题
+	beginBtn := new(menu.Button)
+	beginBtn.SetClickButton("答题", WxBegin)
+	//我的战绩
+	myScore := new(menu.Button)
+	myScore.SetClickButton("我的战绩", WxMyScore)
+	otherBtn.SetSubButton("我的", []*menu.Button{
+		beginBtn,
+		myScore,
+	})
+	//保存到菜单
 	err := m.SetMenu([]*menu.Button{
-		btn1,
-		btn2,
-		btn3,
+		choiceBtn,
+		judgeBtn,
+		otherBtn,
 	})
 	if err != nil {
 		logrus.Error("m.SetMenu:", err)
 	}
 }
 
+//文本消息处理
 func msgTypeText(msg wxMessage.MixMessage, reply *wxMessage.Reply) {
-	reply.MsgType = wxMessage.MsgTypeText
-	reply.MsgData = wxMessage.NewText(fmt.Sprintf("我收到的消息是%s", msg.Content))
+	//判断是不是答题内容
+	for _, item := range []string{WxOptionA, WxOptionB, WxOptionC, WxOptionD, WxOptionE, WxJudgeTrue, WxJudgeFalse} {
+		if msg.Content == item {
+			//答题内容
+			resContent, err := services.Answer(msg.OpenID, item)
+			if err != nil {
+				logrus.Error("services.Answer error:", err)
+				return
+			}
+			reply.MsgType = wxMessage.MsgTypeText
+			reply.MsgData = resContent
+			return
+		}
+	}
 }
 
+//事件处理
 func msgTypeEvent(msg wxMessage.MixMessage, reply *wxMessage.Reply) {
 	switch msg.Event {
 	case wxMessage.EventSubscribe: //关注
@@ -117,7 +166,37 @@ func msgTypeEvent(msg wxMessage.MixMessage, reply *wxMessage.Reply) {
 		reply.MsgType = wxMessage.MsgTypeText
 		reply.MsgData = wxMessage.NewText("后会有期")
 	case wxMessage.EventClick: //点击
+		eventClick(msg, reply)
+	}
+}
+
+//菜单点击事件
+func eventClick(msg wxMessage.MixMessage, reply *wxMessage.Reply) {
+	switch msg.EventKey {
+	case WxOptionA:
+	case WxOptionB:
+	case WxOptionC:
+	case WxOptionD:
+	case WxOptionE:
+	case WxJudgeTrue:
+	case WxJudgeFalse:
+		//答题状态
+		resContent, err := services.Answer(msg.OpenID, msg.EventKey)
+		if err != nil {
+			logrus.Error("services.Answer error:", err)
+			return
+		}
 		reply.MsgType = wxMessage.MsgTypeText
-		reply.MsgData = wxMessage.NewText(fmt.Sprintf("点击的事件是:%s", msg.EventKey))
+		reply.MsgData = resContent
+		return
+	case WxBegin: //开始答题
+		reply.MsgType = wxMessage.MsgTypeText
+		reply.MsgData = "开始答题"
+		return
+
+	case WxMyScore: //我的战绩
+		reply.MsgType = wxMessage.MsgTypeText
+		reply.MsgData = "我的战绩"
+		return
 	}
 }
---
+++ services/wx_question.go
@@ -0,0 +1,74 @@
+package services
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+)
+
+const wxMessageBr = "<br>"
+
+/**
+判断是否微信答题状态
+@param string openid
+@return bool
+*/
+func IsExamination(openid string) bool {
+	logrus.Info(openid)
+	return true
+}
+
+/**
+保存微信答题状态，openid => questionId
+@param string openid
+@param uint questionId 问题ID
+*/
+func SetQuestionForOpenid(openid string, questionId uint) error {
+	//TODO 具体保存方式，待定，mysql容易，redis靠谱
+	return nil
+}
+
+/**
+读取微信答题状态，openid => questionId
+@param string openid
+@return uint questionId 问题ID
+*/
+func GetQuestionForOpenid(openid string) (questionId uint, err error) {
+	//TODO
+	return 1, nil
+}
+
+/**
+下一题
+*/
+func NextQuestion(openid string) (string, error) {
+	//TODO
+	nextQuestionId := 2
+	err := SetQuestionForOpenid(openid, uint(nextQuestionId))
+	if err != nil {
+		return "", err
+	}
+	return "以下哪些时候1111111111", nil
+}
+
+/**
+微信答题
+@param string openid
+@param string answer 答案
+@return string
+*/
+func Answer(openid string, answer string) (string, error) {
+	if !IsExamination(openid) {
+		errorMsg := "未开启答题状态"
+		return errorMsg, fmt.Errorf(errorMsg)
+	}
+	res := ""
+	//判断答案 TODO
+	res += fmt.Sprintf("回答正确~~~%s", wxMessageBr)
+	//下一题
+	nextQuestionStr, err := NextQuestion(openid)
+	if !IsExamination(openid) {
+		return "查询题目错误", err
+	}
+	res += nextQuestionStr
+	return res, nil
+}
