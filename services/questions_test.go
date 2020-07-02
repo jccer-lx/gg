2e21008a43a3ca0deac2588621abaaf4f9b71da0
lvxin <lvxin0315@163.com>
2020-07-02 15:56:20 +0800

    添加题目打乱顺序

--- services/questions_test.go
+++ services/questions_test.go
@@ -5,7 +5,10 @@ import (
 	"github.com/lvxin0315/gg/databases"
 	"github.com/lvxin0315/gg/models"
 	"github.com/sirupsen/logrus"
+	"math/rand"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestAddChoiceQuestionForBank(t *testing.T) {
@@ -22,11 +25,15 @@ func TestAddAllChoiceQuestionForBank(t *testing.T) {
 	databases.InitMysqlDB()
 	logrus.SetLevel(logrus.DebugLevel)
 	var choiceQuestionList []*models.ChoiceQuestion
+
 	err := databases.NewDB().Where("id > 1").Find(&choiceQuestionList).Error
 	if err != nil {
 		fmt.Println(err)
 		t.Fail()
 	}
+	//打乱顺序
+	randSlice(choiceQuestionList)
+
 	for _, cq := range choiceQuestionList {
 		err := AddChoiceQuestionForBank(cq.ID)
 		if err != nil {
@@ -48,4 +55,27 @@ func TestGetQuestionByBankId(t *testing.T) {
 	fmt.Println(questionBankModel.QuestionType)
 	fmt.Println(questionBankModel.Question.GetId())
 	fmt.Println(questionBankModel.Question.(*models.ChoiceQuestion).Stem)
+}
+
+//打乱切片顺序
+func randSlice(slice interface{}) {
+	rv := reflect.ValueOf(slice)
+	if rv.Type().Kind() != reflect.Slice {
+		fmt.Println("rv.Type().Kind() != reflect.Slice")
+		return
+	}
+
+	length := rv.Len()
+	if length < 2 {
+		fmt.Println("length := rv.Len()")
+		return
+	}
+
+	swap := reflect.Swapper(slice)
+	rand.Seed(time.Now().Unix())
+	for i := length - 1; i >= 0; i-- {
+		j := rand.Intn(length)
+		swap(i, j)
+	}
+	return
 }
